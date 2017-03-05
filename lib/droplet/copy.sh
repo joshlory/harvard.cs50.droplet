2836b3620bae6aa56f56d05828c6c4d7cf4bffac
David Anthony Bau <dab1998@gmail.com>
2017-03-05 17:08:46 -0500

    New droplet with getReverse

---
+++ lib/droplet/copy.sh
@@ -0,0 +1,7 @@
+cp ~/Projects/droplet/css/droplet.css .
+cp ~/Projects/droplet/dist/droplet-full.js .
+cp ~/Projects/droplet/dist/worker.js .
+uglify -s droplet-full.js -o droplet-full.min.js
+uglify -s worker.js -o worker.min.js
+mv droplet-full.min.js droplet-full.js
+mv worker.min.js worker.js
